52d7b96fc1bbf07bf168200f122103aeb617d804
Bernardo Copstein <bernardo.copstein@pucrs.br>
2020-02-04 11:31:56 -0300

    Segunda contribuicao

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Minha contribuição");
+    System.out.println("My second contribution");
   }
 }
